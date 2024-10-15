ee2df489a10bfbc384627f8b3040c90f7d02d48e
None083 <perezgonzaleznoelia08@gmail.com>
2024-10-15 14:42:22 +0200

    ficheros

--- Practica 1b - Subir imagen/vistas/vistaphp.php
+++ Practica 1b - Subir imagen/vistas/vistaphp.php
@@ -1,22 +1,38 @@
 <?php
-    echo "<h1>Datos recogidos</h1>";
-    //var_dump($_POST);
-    echo "<p><strong>Nombre: </strong>",$_POST["nombre"]."</p>";
-    echo "<p><strong>Apellidos: </strong>",$_POST["apellidos"]."</p>";
-    echo "<p><strong>Contraseña: </strong>",$_POST["pass"]."</p>";
-    echo "<p><strong>DNI: </strong>",$_POST["dni"]."</p>";
-    echo "<p><strong>Sexo: </strong>";
-    if (isset($_POST["sexo"])) {
-        echo $_POST["sexo"];
-    }
-    echo "</p>";  
-    echo "<p><strong>Nacido en: </strong>",$_POST["ciudad"]."</p>";
-    echo "<p><strong>Comentarios: </strong>",$_POST["coment"]."</p>";
-    echo "<p><strong>Suscrito: </strong>";
-    if (isset($_POST["suscribir"])) {
-        echo "sí";
-    }  else{
-        echo "no";
-    }
-    echo "</p>";
-?>
+echo "<h1>Datos recogidos</h1>";
+//var_dump($_POST);
+echo "<p><strong>Nombre: </strong>", $_POST["nombre"] . "</p>";
+echo "<p><strong>Apellidos: </strong>", $_POST["apellidos"] . "</p>";
+echo "<p><strong>Contraseña: </strong>", $_POST["pass"] . "</p>";
+echo "<p><strong>DNI: </strong>", $_POST["dni"] . "</p>";
+echo "<p><strong>Sexo: </strong>";
+if (isset($_POST["sexo"])) {
+    echo $_POST["sexo"];
+}
+echo "</p>";
+$numero_unico = md5(uniqid(uniqid(), true));
+$ext = tiene_extension($_FILES["foto"]["name"]);
+$nombre_imagen = "img_ " . $numero_unico . "." . $ext;
+
+@$var = move_uploaded_file($_FILES["foto"]["tmp_name"], "images/" . $nombre_imagen);
+echo "<p><strong>Imagen:</strong></p>";
+if (!$var) {
+    echo "<p>No se ha podido mover la imagen a la carpeta destino en el servidor</p>";
+} else {
+    //sudo chmod 777 -R '/opt/lampp/htdocs/Proyectos'
+
+    echo "<p>Nombre orginal: " . $_FILES["foto"]["name"] . "</p>";
+    echo "<p>Tipo: " . $_FILES["foto"]["type"] . "</p>";
+    echo "<p>Tamaño: " . $_FILES["foto"]["size"] . "</p>";
+    echo "<p>Archivo subido temporalmente en: " . $_FILES["foto"]["tmp_name"] . "</p>";
+    echo "<img src='images/$nombre_imagen' alt='Imagen subida' name = 'Imagen subida' width='200px' height='auto'>";
+}
+echo "<p><strong>Nacido en: </strong>", $_POST["ciudad"] . "</p>";
+echo "<p><strong>Comentarios: </strong>", $_POST["coment"] . "</p>";
+echo "<p><strong>Suscrito: </strong>";
+if (isset($_POST["suscribir"])) {
+    echo "sí";
+} else {
+    echo "no";
+}
+echo "</p>";
---
+++ Teoria/teor_ficheros_texto.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teoría Ficheros de Texto</title>
+</head>
+
+<body>
+    <?php
+    @$file = fopen("prueba.txt", "r");
+    //abrir fichero, r lectura, w escritura, a añadir
+    //w: si existe borra el contenido y empieza a escribir en la primera linea
+    //si no existe crea un archivo nuevo
+    if (!$file) {
+        die("<p>No se ha podido abrir el fichero 'prueba.txt'</p>");
+    }
+
+    while (!feof($file)) {
+        $linea = fgets($file);
+        echo "<p>" . $linea . "</p>";
+    }
+
+    echo "<h2>Fin del fichero</h2>";
+
+    fseek($file, 0);
+
+    echo "<h2>Vuelvo a recorrer el fichero</h2>";
+
+    while (!feof($file)) {
+        $linea = fgets($file);
+        echo "<p>" . $linea . "</p>";
+    }
+
+    echo "<h2>Fin del fichero</h2>";
+
+    fclose($file);
+
+    @$file = fopen("prueba.txt", "a");
+    if (!$file) {
+        die("<p>No se ha podido abrir el fichero 'prueba.txt'</p>");
+    }
+
+    fputs($file, PHP_EOL . "Cuarta línea.");
+    fwrite($file, PHP_EOL . "Quinta línea.");
+
+    fclose($file);
+
+    echo "<h2>Lectura de un fichero nuevo</h2>";
+
+    $todo = file_get_contents("prueba.txt");
+
+    echo "<pre>".$todo."</pre>";
+    echo nl2br($todo);
+    echo "<h3>Lectura de una web</h3>";
+    $web = file_get_contents("https://www.google.es");
+    echo nl2br($web);
+    //file_exists("Tablas/".);
+
+    ?>
+</body>
+
+</html>
