0342c7afc0b064fff0053b79fa2563178c8b66ed
None083 <perezgonzaleznoelia08@gmail.com>
2024-10-18 10:20:45 +0200

    ejers exam

---
+++ Examen_22_prueba/ejer1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejer 1</title>
+</head>
+
+<body>
+    <form action="ejer1.php" method="post">
+        <p>Cuenta los caracteres del texto introducido:</p>
+        <textarea name="texto" id="texto"></textarea>
+        <button type="submit" name="contar">Contar</button>
+    </form>
+    <?php
+    if (isset($_POST["contar"])) {
+        $texto = $_POST["texto"];
+        function mi_strlen($texto)
+        {
+            $cont = 0;
+            while (isset($texto[$cont])) {
+                $cont++;
+            }
+            return $cont;
+        }
+        echo "<p>El texto tiene " .mi_strlen($texto). " caracteres</p>";
+    }
+    
+    ?>
+</body>
+
+</html>
