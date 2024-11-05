ceb7050eb386fdf4a8570e2765d58c90797ebff0
None083 <perezgonzaleznoelia08@gmail.com>
2024-11-05 14:44:03 +0100

    muchas cosas de crud

---
+++ primer_crud/vistas/vista_detalle.php
@@ -0,0 +1,18 @@
+<?php
+if (mysqli_num_rows($detalle_usuario) > 0) {
+
+    $tupla_detalles = mysqli_fetch_assoc($detalle_usuario);
+
+    echo "<h2>Detalles del usuario " . $_POST["btnDetalles"] . "</h2>";
+
+    echo "<p>";
+    echo "<strong>Nombre: </strong>" . $tupla_detalles["nombre"] . "</br>";
+    echo "<strong>Nombre: </strong>" . $tupla_detalles["usuario"] . "</br>";
+    echo "<strong>Clave: </strong></br>";
+    echo "<strong>Nombre: </strong>" . $tupla_detalles["email"] . "</br>";
+    echo "</p>";
+} else {
+    echo "<p>El usuario ya no se encuentra registrado en la BD</p>";
+}
+mysqli_free_result($detalle_usuario);
+?>
---
+++ primer_crud/vistas/vista_tabla.php
@@ -0,0 +1,28 @@
+<?php
+echo "<table>";
+    echo "<tr>
+        <th>Nombre de Usuario</th>
+        <th>Borrar</th>
+        <th>Editar</th>
+    </tr>";
+
+    while ($tupla = mysqli_fetch_assoc($datos_usuario)) {
+    echo "<tr>";
+        echo "<td>
+            <form action='index.php' method='post'><button class='enlace' title='Ver detalles' type='submit' name='btnDetalles' value='" . $tupla["id_usuario"] . "'>" . $tupla["nombre"] . "</form>
+        </td>";
+        echo "<td>
+            <form action='index.php' method='post'><button class='btn_imagen' type='submit' name='btnBorrar' value='" . $tupla["id_usuario"] . "'>Borrar</button></form>
+        </td>";
+        echo "<td>Editar</td>";
+        echo "</tr>";
+    }
+    echo "<tr>
+        <td colspan='3'>
+            <form action='index.php' method='post'><button type='submit' class='btn_agregar' name='btnAgregar'>Agregar usuario</button></form>
+        </td>
+    </tr>";
+
+echo "</table>";
+mysqli_free_result($datos_usuario);
+?>
