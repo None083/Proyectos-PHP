b8a2e4f5b6a8403f9a701c618db9f0ada3eed9a1
Noelia <perezgonzaleznoelia08@gmail.com>
2024-11-06 22:33:51 +0100

    crud agregar

--- primer_crud/vistas/vista_errores.php
+++
@@ -1,7 +0,0 @@
-<?php
-$error_nombre_vacio = isset($_POST["nombre"]) == "";
-$error_usuario_vacio = isset($_POST["usuario"]) == "";
-$error_clave_vacio = isset($_POST["clave"]) == "";
-$error_email_vacio = isset($_POST["email"]) == "";
-
-?>
