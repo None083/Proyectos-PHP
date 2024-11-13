316842a307f4437ec33204a58a53a91d955ce79a
Noelia <perezgonzaleznoelia08@gmail.com>
2024-11-13 18:12:33 +0100

    no sale editar, cosorro

--- Practica 8/index.php
+++ Practica 8/index.php
@@ -7,13 +7,28 @@ try {
     @$conexion = mysqli_connect(SERVIDOR_BD, USUARIO_BD, CLAVE_BD, NOMBRE_BD);
     mysqli_set_charset($conexion, "utf8");
 } catch (Exception $e) {
-    mysqli_close($conexion);
-    session_destroy();
     die(error_page("Práctica 8", "<p>No se ha podido conectar a la BD: " . $e->getMessage() . "</p>"));
 }
 //a partir de aqui tengo conxion con mi bd
 
-if (isset($_POST["btnDetalles"]) || isset($_POST["btnEnviar"]) || isset($_POST["btnBorrar"])) {
+if (isset($_POST["btnContBorrar"])) {
+    try {
+        $consulta = "delete from usuarios where id_usuario='" . $_POST["btnContBorrar"] . "'";
+        mysqli_query($conexion, $consulta);
+        if ($_POST["nombre_foto_bd"] != NOMBRE_IMAGEN_DEFECTO_BD) {
+            unlink("Img/" . $_POST["nombre_foto_bd"]);
+        }
+        $_SESSION["mensaje_accion"] = "Usuario borrado con éxito";
+        header("Location:index.php");
+        exit;
+    } catch (Exception $e) {
+        mysqli_close($conexion);
+        session_destroy();
+        die(error_page("Práctica 8", "<p>No se ha podido realizar la consulta: " . $e->getMessage() . "</p>"));
+    }
+}
+
+if (isset($_POST["btnDetalles"]) || isset($_POST["btnBorrar"]) || isset($_POST["btnEditar"])) {
     if (isset($_POST["btnDetalles"])) {
         $id_usuario = $_POST["btnDetalles"];
     } else {
@@ -22,21 +37,11 @@ if (isset($_POST["btnDetalles"]) || isset($_POST["btnEnviar"]) || isset($_POST["
 
     try {
         $consulta = "select * from usuarios where id_usuario='" . $id_usuario . "'";
-        $detalle_usuario = mysqli_query($conexion, $consulta);
-    } catch (Exception $e) {
-        die(error_page("Práctica 8", "<p>No se ha podido realizar la consulta: " . $e->getMessage() . "</p>"));
-    }
-}
-
-if (isset($_POST["btnContBorrar"])) {
-    try {
-        $consulta = "delete from usuarios where id_usuario='" . $_POST["btnContBorrar"] . "'";
-        mysqli_query($conexion, $consulta);
-        $_SESSION["mensaje_accion"] = "Usuario borrado con éxito";
-        header("Location:index.php");
-        exit;
+        $result_detalle_usuario = mysqli_query($conexion, $consulta);
+        $detalle_usuario = mysqli_fetch_assoc($result_detalle_usuario);
     } catch (Exception $e) {
         mysqli_close($conexion);
+        session_destroy();
         die(error_page("Práctica 8", "<p>No se ha podido realizar la consulta: " . $e->getMessage() . "</p>"));
     }
 }
@@ -54,22 +59,125 @@ if (isset($_POST["btnContCrear"])) {
         }
     }
     $error_clave = $_POST["clave"] == "";
-    $error_dni = !dni_valido($_POST["dni"]);
+    $error_dni = $_POST["dni"] == "" || !dni_bien_escrito($_POST["dni"]) || !dni_valido($_POST["dni"]);
     if (!$error_dni) {
-        $error_dni = repetido($conexion, "usuarios", "usuario", strtoupper($_POST["dni"]));
+        $error_dni = repetido($conexion, "usuarios", "dni", strtoupper($_POST["dni"]));
         if (is_string($error_dni)) {
             mysqli_close($conexion);
             session_destroy();
             die(error_page("Primer CRUD", "<p>" . $error_dni . "</p>"));
         }
     }
+
     $error_foto = $_FILES["foto"]["name"] != "" && ($_FILES["foto"]["error"] || !tiene_extension($_FILES["foto"]["name"]) || !getimagesize($_FILES["foto"]["tmp_name"]) || $_FILES["foto"]["size"] > 500 * 1024);
 
     $errores_form_crear = $error_nombre || $error_usuario || $error_dni || $error_foto || $error_clave;
 
     if (!$errores_form_crear) {
         //inserto foto por defecto
-        //y si he subido foto, muevo la fotro y actualizo el nombre de la foto en la bd (img_id.extension)
+        //y si he subido foto, muevo la foto y actualizo el nombre de la foto en la bd (img_id.extension)
+
+        try {
+            $consulta = "insert into usuarios (nombre, usuario, clave, dni, sexo) values ('" . $_POST["nombre"] . "','" . $_POST["usuario"] . "','" . md5($_POST["clave"]) . "','" . strtoupper($_POST["dni"]) . "','" . $_POST["sexo"] . "')";
+            mysqli_query($conexion, $consulta);
+        } catch (Exception $e) {
+            mysqli_close($conexion);
+            session_destroy();
+            die(error_page("Práctica 8", "<p>No se ha podido realizar la consulta: " . $e->getMessage() . "</p>"));
+        }
+
+        $_SESSION["mensaje_accion"] = "Usuario insertado con éxito.";
+        if ($_FILES["foto"]["name"] != "") {
+            $ultm_id = mysqli_insert_id($conexion);
+            $array_nombre = explode(".", $_FILES["foto"]["name"]);
+            $ext = end($array_nombre);
+            $nombre_nuevo = "img_" . $ultm_id . "." . $ext;
+            @$var = move_uploaded_file($_FILES["foto"]["tmp_name"], "Img/" . $nombre_nuevo);
+            if ($var) {
+                try {
+                    $consulta = "update usuarios set foto='" . $nombre_nuevo . "' where id_usuario='" . $ultm_id . "'";
+                    mysqli_query($conexion, $consulta);
+                } catch (Exception $e) {
+                    unlink("Img/" . $nombre_nuevo);
+                    $_SESSION["mensaje_accion"] = "Usuario insertado con éxito, pero con la imagen por defecto.";
+                }
+            } else {
+                $_SESSION["mensaje_accion"] = "Usuario insertado con éxito, pero con la imagen por defecto.";
+            }
+        }
+
+        header("Location:index.php");
+        exit();
+    }
+}
+
+if (isset($_POST["btnContEditar"])) {
+    $error_nombre = $_POST["nombre"] == "";
+
+    $error_usuario = $_POST["usuario"] == "";
+    if (!$error_usuario) {
+        $error_usuario = repetido($conexion, "usuarios", "usuario", $_POST["usuario"], "id_usuario", $_POST["btnContEditar"]);
+        if (is_string($error_usuario)) {
+            mysqli_close($conexion);
+            session_destroy();
+            die(error_page("Primer CRUD", "<p>" . $error_usuario . "</p>"));
+        }
+    }
+
+    $error_clave = $_POST["clave"] == "";
+
+    $error_dni = $_POST["dni"] == "" || !dni_bien_escrito($_POST["dni"]) || !dni_valido($_POST["dni"]);
+    if (!$error_dni) {
+        $error_dni = repetido($conexion, "usuarios", "dni", $_POST["dni"], "id_usuario", $_POST["btnContEditar"]);
+        if (is_string($error_dni)) {
+            mysqli_close($conexion);
+            session_destroy();
+            die(error_page("Primer CRUD", "<p>" . $error_dni . "</p>"));
+        }
+    }
+
+    $error_foto = $_FILES["foto"]["name"] != "" && ($_FILES["foto"]["error"] || !tiene_extension($_FILES["foto"]["name"]) || !getimagesize($_FILES["foto"]["tmp_name"]) || $_FILES["foto"]["size"] > 500 * 1024);
+
+    $errores_form_editar = $error_nombre || $error_usuario || $error_dni || $error_foto || $error_clave;
+
+    //Si no hay errores edito en la tabla e informo de la acción
+    if (!$errores_form_editar) {
+        //Edito y mensaje
+        try {
+            if ($_POST["clave"] == "") {
+                $consulta = "update usuarios set nombre='" . $_POST["nombre"] . "', usuario='" . $_POST["usuario"] . "', dni='" . $_POST["dni"] . "', sexo='" . $_POST["sexo"] . "' where id_usuario='" . $_POST["btnContEditar"] . "'";
+            } else {
+                $consulta = "update usuarios set nombre='" . $_POST["nombre"] . "', usuario='" . $_POST["usuario"] . "', clave='" . md5($_POST["clave"]) . "', dni='" . $_POST["dni"] . "', sexo='" . $_POST["sexo"] . "' where id_usuario='" . $_POST["btnContEditar"] . "'";
+            }
+            if ($_FILES["foto"]["name"] != "") {
+                $ultm_id = mysqli_insert_id($conexion);
+                $array_nombre = explode(".", $_FILES["foto"]["name"]);
+                $ext = end($array_nombre);
+                $nombre_nuevo = "img_" . $ultm_id . "." . $ext;
+                @$var = move_uploaded_file($_FILES["foto"]["tmp_name"], "Img/" . $nombre_nuevo);
+                if ($var) {
+                    try {
+                        $consulta = "update usuarios set foto='" . $nombre_nuevo . "' where id_usuario='" . $ultm_id . "'";
+                        mysqli_query($conexion, $consulta);
+                    } catch (Exception $e) {
+                        unlink("Img/" . $nombre_nuevo);
+                        $_SESSION["mensaje_accion"] = "Usuario insertado con éxito, pero con la imagen por defecto.";
+                    }
+                } else {
+                    $_SESSION["mensaje_accion"] = "Usuario insertado con éxito, pero con la imagen por defecto.";
+                }
+            }
+
+            header("Location:index.php");
+            exit();
+            mysqli_query($conexion, $consulta);
+            $_SESSION["mensaje_accion"] = "Usuario editado con éxito";
+            header("Location:index.php");
+            exit;
+        } catch (Exception $e) {
+            mysqli_close($conexion);
+            die(error_page("Primer CRUD", "<p>No se ha podido realizar la consulta: " . $e->getMessage() . "</p>"));
+        }
     }
 }
 
@@ -115,6 +223,10 @@ mysqli_close($conexion);
             font-size: 1.25rem;
             color: blue;
         }
+
+        .error {
+            color: red;
+        }
     </style>
     <title>CRUD - PRÁCTICA 8</title>
 </head>
@@ -138,6 +250,10 @@ mysqli_close($conexion);
 
     if (isset($_POST["btnCrear"]) || (isset($_POST["btnContCrear"]) && $errores_form_crear)) {
         require "vistas/vista_crear.php";
+    }
+
+    if (isset($_POST["btnEditar"]) || (isset($_POST["btnContEditar"]) && $errores_form_editar)) {
+        require "vistas/vista_editar.php";
     }
 
     require "vistas/vista_tabla.php";
--- Practica 8/vistas/vista_editar.php
+++ Practica 8/vistas/vista_editar.php
@@ -1,19 +1,45 @@
-<h2>Creando un nuevo usuario</h2>
+<?php
+if(isset($_POST["btnEditar"]))
+{
+    if(isset($detalle_usuario))
+    {
+        $nombre=$detalle_usuario["nombre"];
+        $usuario=$detalle_usuario["usuario"];
+        $dni=$detalle_usuario["dni"];
+        $sexo=$detalle_usuario["sexo"];
+    }
+    else
+    {
+        die("<p>El usuario ya no se encuentra registrado en la BD</p></body></html>");
+
+    }
+}
+else
+{
+    $id_usuario=$_POST["btnContEditar"];
+    $nombre=$_POST["nombre"];
+    $usuario=$_POST["usuario"];
+    $dni=$_POST["dni"];
+    $sexo=$_POST["sexo"];
+}
+
+?>
+<h2>Editando el usuario <?php echo $id_usuario;?></h2>
 <form action="index.php" method="post" enctype="multipart/form-data">
     <p>
         <label for="nombre">Nombre:</label></br>
-        <input type="text" name="nombre" id="nombre" placeholder="Nombre..." value="<?php if (isset($_POST["nombre"])) echo $_POST["nombre"]; ?>">
+        <input type="text" name="nombre" id="nombre" placeholder="Nombre..." value="<?php echo $nombre; ?>">
         <?php
-        if (isset($_POST["btnContCrear"]) && $error_nombre) {
+        if (isset($_POST["btnContEditar"]) && $error_nombre) {
             echo "<span class='error'> * Campo obligatorio * </span>";
         }
         ?>
     </p>
     <p>
         <label for="usuario">Usuario:</label></br>
-        <input type="text" name="usuario" id="usuario" placeholder="Usuario..." value="<?php if (isset($_POST["usuario"])) echo $_POST["usuario"]; ?>">
+        <input type="text" name="usuario" id="usuario" placeholder="Usuario..." value="<?php echo $usuario; ?>">
         <?php
-        if (isset($_POST["btnContCrear"]) && $error_usuario) {
+        if (isset($_POST["btnContEditar"]) && $error_usuario) {
             if ($_POST["usuario"] == "") {
                 echo "<span class='error'> * Campo obligatorio * </span>";
             } else {
@@ -25,17 +51,12 @@
     <p>
         <label for="clave">Clave:</label></br>
         <input type="password" name="clave" id="clave" placeholder="Clave..." value="">
-        <?php
-        if (isset($_POST["btnContAgregar"]) && $error_clave) {
-            echo "<span class='error'>* Campo vacío *</span>";
-        }
-        ?>
     </p>
     <p>
         <label for="dni">DNI:</label></br>
-        <input type="text" name="dni" id="dni" placeholder="DNI..." value="<?php if (isset($_POST["dni"])) echo $_POST["dni"]; ?>">
+        <input type="text" name="dni" id="dni" placeholder="DNI..." value="<?php echo $dni; ?>">
         <?php
-        if (isset($_POST["btnContCrear"]) && $error_dni) {
+        if (isset($_POST["btnContEditar"]) && $error_dni) {
             if ($_POST["dni"] == "") {
                 echo "<span class='error'> * Campo Vacío *</span>";
             } elseif (!dni_bien_escrito($_POST["dni"])) {
@@ -50,14 +71,14 @@
     </p>
     <p>
         Sexo:</br>
-        <input type="radio" name="sexo" id="hombre" value="hombre" checked><label for="hombre">Hombre</label></br>
-        <input type="radio" name="sexo" id="mujer" value="mujer" <?php if (isset($_POST["sexo"]) && $_POST["sexo"] == "mujer") echo "checked"; ?>><label for="mujer">Mujer</label>
+        <input type="radio" name="sexo" id="hombre" value="hombre" <?php if($sexo == "hombre") echo "checked"; ?>><label for="hombre">Hombre</label></br>
+        <input type="radio" name="sexo" id="mujer" value="mujer" <?php if($sexo == "mujer") echo "checked"; ?>><label for="mujer">Mujer</label>
     </p>
     <p>
-        <label for="foto">Incluir mi foto (Max. 500KB):</label>
+        <label for="foto">Cambiar mi foto (Max. 500KB):</label>
         <input type="file" name="foto" id="foto" accept="image/*">
         <?php
-        if (isset($_POST["btnContCrear"]) && $error_foto) {
+        if (isset($_POST["btnContEditar"]) && $error_foto) {
             if ($_FILES["foto"]["error"])
                 echo "<span class='error'>* No se ha subido el archivo seleccionado al servidor *</span>";
             elseif (!tiene_extension($_FILES["foto"]["name"]))
@@ -70,7 +91,7 @@
         ?>
     </p>
     <p>
-        <button type="submit" name="btnContCrear">Guardar</button>
+        <button type="submit" name="btnContEditar">Guardar</button>
         <button type="submit">Atrás</button>
     </p>
 </form>
