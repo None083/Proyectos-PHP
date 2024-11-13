332450a727bb5188766f2834f44dd2fda8eeef3a
Noelia <perezgonzaleznoelia08@gmail.com>
2024-11-13 18:12:33 +0100

    no sale editar, cosorro

--- Practica 8/vistas/vista_borrar.php
+++ Practica 8/vistas/vista_borrar.php
@@ -1,10 +1,12 @@
 <?php
 echo "<h2>Borrado del usuario " . $_POST["id_usuario"] . "</h2>";
-if (mysqli_num_rows($detalle_usuario) > 0) {
-    $tupla_detalles = mysqli_fetch_Assoc($detalle_usuario);
-    echo "<p>¿Estás seguro de que quieres borrar al usuario <strong>".$tupla_detalles["nombre"]."</strong>?</p>";
+if (isset($detalle_usuario)) {
+    echo "<p>¿Estás seguro de que quieres borrar al usuario <strong>".$detalle_usuario["nombre"]."</strong>?</p>";
     echo "<form action='index.php' method='post'>";
-    echo "<button type='submit' name='btnContBorrar' value='".$_POST["btnBorrar"]."'>Continuar</button>";
+    echo "<input type='hidden' name='nombre_foto_bd' value='".$detalle_usuario["foto"]."'/>";
+    echo "<button type='submit' name='btnContBorrar' value='".$id_usuario."'>Continuar</button>";
     echo "<button type='submit'>Atrás</button>";
     echo "</form>";
+}else{
+    echo "<p>El usuario ya no se encuentra registrado en la BD</p>";
 }
