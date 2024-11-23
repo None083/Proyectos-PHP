fbe45aeb9f6b233173483ac16c81b02493d55699
Noelia <perezgonzaleznoelia08@gmail.com>
2024-11-23 18:16:51 +0100

    cosas de js

--- dwese/Tema2/Enunciado_Examen2_23_24/Examen2/index.php
+++ dwese/Tema2/Enunciado_Examen2_23_24/Examen2/index.php
@@ -1,22 +1,33 @@
 <?php
-
 session_start();
-
 require "src/funciones_const.php";
 
+//Conexión con BD
 try {
     @$conexion = mysqli_connect(SERVIDOR_BD, USUARIO_BD, CLAVE_BD, NOMBRE_BD);
     mysqli_set_charset($conexion, "utf8");
 } catch (Exception $e) {
     die(error_page("Examen 2", "<p>No se ha podido conectar a la BD: " . $e->getMessage() . "</p>"));
 }
-//a partir de aqui tengo conxion con mi bd
+
+// Consulta para obtener los datos del horario del profesor
+if (isset($_POST["verHorario"])) {
+    try {
+        $id_usuario = $_POST["select_usuarios"];
+        $consulta = "
+                select horario_lectivo.dia, horario_lectivo.hora, grupos.id_grupo, grupos.nombre, usuarios.id_usuario from usuarios
+                join horario_lectivo on horario_lectivo.usuario = usuarios.id_usuario
+                join grupos on grupos.id_grupo = horario_lectivo.grupo
+                where id_usuario = " . $id_usuario . ";";
+
+        $result_horario = mysqli_query($conexion, $consulta);
+    } catch (Exception $e) {
+        die(error_page("Práctica examen 2", "<p>No se ha podido realizar la consulta: " . $e->getMessage() . "</p>"));
+    }
+}
 
 
-
-
-
-//consulta para listar usuarios
+// Consulta para listar usuarios
 try {
     $consulta = "select * from usuarios";
     $datos_usuario = mysqli_query($conexion, $consulta);
@@ -32,6 +43,27 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table,
+        td,
+        th {
+            border: 1px solid black;
+        }
+
+        table {
+            border-collapse: collapse;
+            text-align: center;
+            width: 90%;
+            margin: 0 auto;
+        }
+        .enlace {
+            background: none;
+            color: blue;
+            cursor: pointer;
+            border: none;
+            text-decoration: underline;
+        }
+    </style>
     <title>Examen2 PHP</title>
 </head>
 
@@ -52,8 +84,42 @@ try {
         </p>
     </form>
     <?php
-    if (isset($_POST["verHorario"])) {
-        
+    if (isset($_POST["verHorario"]) || isset($_POST["btnEditar"])) {
+        $resultado = [];
+        while ($tupla = mysqli_fetch_assoc($result_horario)) {
+            $resultado[] = $tupla;
+        }
+        $horas = ["8:15 - 9:15", "9:15 - 10:15", "10:15 - 11:15", "11:15 - 11:45", "11:45 - 12:45", "12:45 - 13:45", "13:45 - 14:45"];
+        echo "<table>";
+        echo "<tr><th></th><th>Lunes</th><th>Martes</th><th>Miércoles</th><th>Jueves</th><th>Viernes</th></tr>";
+        for ($hora = 0; $hora < count($horas); $hora++) {
+            echo "<tr><th>" . $horas[$hora] . "</th>";
+            if ($hora == 3) {
+                echo "<td colspan='5'>RECREO</td>";
+            } else {
+                for ($dia = 0; $dia < 5; $dia++) {
+                    $grupos="";
+                    echo "<td>";
+                    foreach ($resultado as $value) {
+                        if ($value["dia"] == $dia + 1 && $value["hora"] == $hora + 1) {
+                            if ($grupos == "") {
+                                $grupos = $value["nombre"];
+                            }else{
+                                $grupos .= " / " . $value["nombre"];
+                            }
+                        }
+                    }
+                    echo $grupos;
+                    echo "<form action='index.php' method='post'><button type='submit' class='enlace' name='btnEditar' value='".$value["id_usuario"]."'>Editar</button><input type='hidden' name='hora' value='".$hora."'><input type='hidden' name='dia' value='".$dia."'></form>";
+                    echo "</td>";
+                }
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+        if (isset($_POST["btnEditar"])) {
+            echo $_POST["hora"];
+        }
     }
     ?>
 </body>
