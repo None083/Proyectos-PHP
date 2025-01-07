cd56e787a2da3946830912772c1778e920546c4e
None083 <perezgonzaleznoelia08@gmail.com>
2025-01-07 14:43:14 +0100

    teor sw

--- dwese/Tema3/SW/Teor_SW/API/index.php
+++ dwese/Tema3/SW/Teor_SW/API/index.php
@@ -2,15 +2,42 @@
 
 require __DIR__ . '/Slim/autoload.php';
 
-$app= new \Slim\App;
+$app = new \Slim\App;
 
-$app->get('/saludo/{codigo}',function($request){
+$app->get('/saludo', function () {
 
-    //$datos["cod"]=$request->getParam('cod');
-    echo json_encode(array("mensaje"=> "Hola ".$request->getAttribute('codigo')) ,JSON_FORCE_OBJECT);
-
+    $respuesta["mensaje"] = "Hola que tal?";
+    echo json_encode($respuesta);
 });
 
-// Una vez creado servicios los pongo a disposición
+$app->get('/saludo/{nombre}', function ($request) {
+    $nombre = $request->getAttribute("nombre");
+    $respuesta["mensaje"] = "Hola que tal, " . $nombre . "?";
+    echo json_encode($respuesta);
+});
+
+$app->post('/saludo', function ($request) {
+
+    $nombre = $request->getParam("nombre");
+    $respuesta["mensaje"] = "Hola que tal, " . $nombre . "?";
+    echo json_encode($respuesta);
+});
+
+//delete('/borrar_saludo/{id}')
+$app->delete('/borrar_saludo/{id}', function ($request) {
+    $id = $request->getAttribute("id");
+    $respuesta["mensaje"] = "Saludo con id " . $id . " borrado";
+    echo json_encode($respuesta);
+});
+
+//put('/cambiar_saludo/{id}')
+//por debajo un nombre
+$app->put('/cambiar_saludo/{id}', function ($request) {
+    $id = $request->getAttribute("id");
+    $nombre = $request->getParam("nombre");
+    $respuesta["mensaje"] = "Saludo con id " . $id . " cambiado a " . $nombre;
+    echo json_encode($respuesta);
+});
+
+
 $app->run();
-?>
