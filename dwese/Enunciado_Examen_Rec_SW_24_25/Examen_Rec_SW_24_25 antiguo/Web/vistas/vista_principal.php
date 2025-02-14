cbb52e60b636b6bed887e33210d155e47a983b10
None083 <perezgonzaleznoelia08@gmail.com>
2025-02-14 11:11:44 +0100

    puta mier absoluta

---
+++ dwese/Enunciado_Examen_Rec_SW_24_25/Examen_Rec_SW_24_25 antiguo/Web/vistas/vista_principal.php
@@ -0,0 +1,145 @@
+<?php
+
+$headers[] = 'Authorization: Bearer ' . $_SESSION["token"];
+$url = DIR_SERV . "/usuario/" . $datos_usu_log["id_usuario"];
+$respuesta = consumir_servicios_JWT_REST($url, "GET", $headers);
+$json_usuario = json_decode($respuesta, true);
+if (!$json_usuario) {
+    session_destroy();
+    die(error_page("Gestión de Guardias", "<h1>Gestión de Guardias</h1><p>Error consumiendo el servicio Rest: <strong>" . $url . "</strong></p>"));
+}
+if (isset($json_usuario["error"])) {
+    session_destroy();
+    die(error_page("Gestión de Guardias", "<h1>Gestión de Guardias</h1><p>" . $json_usuario["error"] . "</p>"));
+}
+if (isset($json_usuario["mensaje"])) {
+    $error_usuario = true;
+} else {
+    $_SESSION["token"] = $json_usuario["token"];
+    $_SESSION["ultm_accion"] = time();
+    header("Location:index.php");
+    exit;
+}
+
+/*
+$headers[] = 'Authorization: Bearer ' . $_SESSION["token"];
+$url = DIR_SERV . "/usuariosGuardia/" . urlencode(1) . "/" . urlencode(1);
+$respuesta = consumir_servicios_JWT_REST($url, "GET", $headers);
+$json_usuarios_guardia = json_decode($respuesta, true);
+if (!$json_usuarios_guardia) {
+    session_destroy();
+    die(error_page("Gestión de Guardias", "<h1>Gestión de Guardias</h1><p>Error consumiendo el servicio Rest: <strong>" . $url . "</strong></p>"));
+}
+if (isset($json_usuarios_guardia["error"])) {
+    session_destroy();
+    die(error_page("Gestión de Guardias", "<h1>Gestión de Guardias</h1><p>" . $json_usuarios_guardia["error"] . "</p>"));
+}
+if (isset($json_usuarios_guardia["mensaje"])) {
+    $error_usuario = true;
+} else {
+    $_SESSION["token"] = $json_usuarios_guardia["token"];
+    $_SESSION["ultm_accion"] = time();
+    header("Location:index.php");
+    exit;
+}
+*/
+/*
+$headers[] = 'Authorization: Bearer ' . $_SESSION["token"];
+$url = DIR_SERV . "/deGuardia/" . urlencode($datos_usu_log["id_usuario"]);
+$respuesta = consumir_servicios_JWT_REST($url, "GET", $headers);
+$json_dias_horas_guardia = json_decode($respuesta, true);
+if (!$json_dias_horas_guardia) {
+    session_destroy();
+    die(error_page("Gestión de Guardias", "<h1>Gestión de Guardias</h1><p>Error consumiendo el servicio Rest: <strong>" . $url . "</strong></p>"));
+}
+if (isset($json_dias_horas_guardia["error"])) {
+    session_destroy();
+    die(error_page("Gestión de Guardias", "<h1>Gestión de Guardias</h1><p>" . $json_dias_horas_guardia["error"] . "</p>"));
+}
+if (isset($json_dias_horas_guardia["mensaje"])) {
+    $error_usuario = true;
+} else {
+    $_SESSION["token"] = $json_dias_horas_guardia["token"];
+    $_SESSION["ultm_accion"] = time();
+    header("Location:index.php");
+    exit;
+}
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de Guardias</title>
+    <style>
+        .enlinea {
+            display: inline
+        }
+
+        .enlace {
+            background: none;
+            border: none;
+            color: blue;
+            text-decoration: underline;
+            cursor: pointer;
+        }
+
+        table {
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        table,
+        tr,
+        th,
+        td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Gestión de Guardias</h1>
+    <div>
+        Bienvenido <strong><?php echo $datos_usu_log["usuario"]; ?></strong> - <form class="enlinea" action="index.php" method="post"><button class="enlace" type="submit" name="btnSalir">Salir</button></form>
+    </div>
+    <h2>Equipos de Guardia del IES Mar de Alborán</h2>
+    <?php
+    $dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
+    $horas = ["1º Hora", "2º Hora", "3º Hora", "", "4º Hora", "5º Hora", "6º Hora"];
+    echo $datos_usu_log["id_usuario"];
+    var_dump($json_usuario);
+    //var_dump($json_usuarios_guardia);
+    var_dump($json_dias_horas_guardia);
+
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<th></th>";
+    for ($i = 0; $i < count($dias); $i++)
+        echo "<th>" . $dias[$i] . "</th>";
+    echo "</tr>";
+
+    for ($hora = 0; $hora < count($horas); $hora++) {
+        echo "<tr>";
+        echo "<th>" . $horas[$hora] . "</th>";
+        if ($hora == 3) {
+            echo "<td colspan='5'>RECREO</td>";
+        } else {
+            for ($dia = 1; $dia <= count($dias); $dia++) {
+                echo "<td>";
+                echo "<button type='submit'>Equipo " . ($hora * 5) + $dia . "</button>";
+                echo "</td>";
+            }
+        }
+
+        echo "</tr>";
+    }
+    echo "</table>";
+    ?>
+</body>
+
+</html>
---
+++ dwese/Enunciado_Examen_Rec_SW_24_25/Examen_Rec_SW_24_25 antiguo/servicios_rest/index.php
@@ -0,0 +1,86 @@
+<?php
+
+require __DIR__ . '/Slim/autoload.php';
+
+require "src/funciones_CTES.php";
+
+$app = new \Slim\App;
+
+$app->get('/logueado', function () {
+
+    $test = validateToken();
+    if (is_array($test)) {
+        echo json_encode($test);
+    } else
+        echo json_encode(array("no_auth" => "No tienes permiso para usar el servicio"));
+});
+
+
+$app->post('/login', function ($request) {
+
+    $datos_login[] = $request->getParam("usuario");
+    $datos_login[] = $request->getParam("clave");
+
+
+    echo json_encode(login($datos_login));
+});
+
+
+
+
+/*
+Obtener todos los datos de un usuario, mediane una petición GET. En caso de error por la BD el JSON 
+devuelto será: { “error” : “Error....”}. Si el usuario no se encuentra registrado en la base de datos el JSON 
+será: {“mensaje”: “El usuario no se encuentra registrado en la BD”}, en otro caso el JSON será: { “usuario” : 
+{...}}
+URL de la petición: http://localhost/Proyectos/Examen_Rec_SW_24_25/usuario/{id_usuario}, dónde 
+id_usuario es un atributo pasado por la URL.
+*/
+
+$app->get('/usuario/{id_usuario}', function ($request) {
+    $test = validateToken();
+    if (is_array($test)) {
+        $id_usuario = $request->getAttribute("id_usuario");
+        echo json_encode(obtener_usuario($id_usuario));
+    } else {
+        echo json_encode(array("no_auth" => "No tienes permisos para usar este servicio"));
+    }
+});
+
+
+/* 
+Obtener todos los datos de todos los usuarios que están de guardia un día a una hora mediante una petición 
+GET.  En caso de error por la BD el JSON devuelto será: { “error” : “Error....”}, en otro caso el JSON será: 
+{ “usuarios” :[{...}, {...},...,{...}]}
+URL de la petición: http://localhost/Proyectos/Examen_Rec_SW_24_25/servicios_rest/usuariosGuardia/{dia}/{hora}, 
+dónde dia y hora son dos atributos pasados por la URL.
+*/
+$app->get('/usuariosGuardia/{dia}/{hora}', function ($request) {
+    $test = validateToken();
+    if (is_array($test)) {
+        $dia[] = $request->getParam("dia");
+        $hora[] = $request->getParam("hora");
+        echo json_encode(obtener_usuarios_guardia($dia, $hora));
+    } else {
+        echo json_encode(array("no_auth" => "No tienes permisos para usar este servicio"));
+    }
+});
+
+/*
+Obtener  todos  los  días  y  horas  que  un  usuario  está  de  guardia  mediante  una  petición  GET. En caso 
+de error por la BD el JSON devuelto será: { “error” : “Error....”}, en otro caso el JSON será: { “de_guardia” : 
+[ {dia1,hora1},{dia2,hora2} ,..., {diaN,horaN} ] }
+URL de la petición:http://localhost/Proyectos/Examen_Final_Rec_Junio_23_24/servicios_rest/deGuardia/{id_usuario}, 
+dónde id_usuario es un atributo pasado por la URL.
+*/
+$app->get('/deGuardia/{id_usuario}', function ($request) {
+    $test = validateToken();
+    if (is_array($test)) {
+        $id_usuario[] = $request->getParam("id_usuario");
+        echo json_encode(obtener_dias_horas_guardia($id_usuario));
+    } else {
+        echo json_encode(array("no_auth" => "No tienes permisos para usar este servicio"));
+    }
+});
+
+$app->run();
