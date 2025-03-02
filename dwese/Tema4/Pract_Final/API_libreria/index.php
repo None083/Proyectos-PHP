f6843ecb518257377d6570c8c9c99e324ddb949d
Noelia <perezgonzaleznoelia08@gmail.com>
2025-03-03 00:30:51 +0100

    en la p

--- dwese/Tema4/Pract_Final/API_libreria/index.php
+++ dwese/Tema4/Pract_Final/API_libreria/index.php
@@ -4,51 +4,64 @@ require __DIR__ . '/Slim/autoload.php';
 
 require "src/funciones_CTES.php";
 
-$app = new \Slim\App;
+$app= new \Slim\App;
 
-$app->get('/logueado', function () {
+$app->get('/logueado',function(){
 
-    $test = validateToken();
-    if (is_array($test)) {
+    $test=validateToken();
+    if(is_array($test))
+    {
         echo json_encode($test);
-    } else
-        echo json_encode(array("no_auth" => "No tienes permiso para usar el servicio"));
+    }
+    else
+        echo json_encode(array("no_auth"=>"No tienes permiso para usar el servicio"));  
 });
 
 
-$app->post('/login', function ($request) {
+$app->post('/login',function($request){
+    
+    $datos_login[]=$request->getParam("lector");
+    $datos_login[]=$request->getParam("clave");
 
-    $datos_login[] = $request->getParam("lector");
-    $datos_login[] = $request->getParam("clave");
 
     echo json_encode(login($datos_login));
 });
 
 
-$app->get('/obtenerLibros', function () {
-    echo json_encode(obtener_libros());
+$app->get('/obtenerLibros',function(){
+
+    echo json_encode(obtener_libros());  
 });
 
-$app->get('/obtenerLibro/{referencia}', function ($request) {
-    $test = validateToken();
-    if (is_array($test)) {
-        if (isset($test["usuario"])) {
-            if ($test["usuario"]["tipo"] == "admin") {
-                $respuesta = obtener_libro($request->getAttribute('referencia'));
-                $respuesta["token"] = $test["token"];
+
+$app->get('/obtenerLibro/{referencia}',function($request){
+
+    $test=validateToken();
+    if(is_array($test))
+    {
+        if(isset($test["usuario"]))
+        {
+            if($test["usuario"]["tipo"]=="admin")
+            {
+                $respuesta=obtener_libro($request->getAttribute("referencia"));
+                $respuesta["token"]=$test["token"];
                 
                 echo json_encode($respuesta);
-            } else {
-                echo json_encode(array("no_auth" => "No tienes permiso para usar el servicio"));
             }
-        } else {
-            echo json_encode($test);
+            else
+                echo json_encode(array("no_auth"=>"No tienes permiso para usar el servicio"));  
         }
-    } else {
-        echo json_encode(array("no_auth" => "No tienes permiso para usar el servicio"));
-    }
-});
+        else
+            echo json_encode($test);
 
+    }
+    else
+        echo json_encode(array("no_auth"=>"No tienes permiso para usar el servicio"));  
+
+    
+
+   
+});
 
 $app->post('/crearLibro', function ($request) {
     $test = validateToken();
@@ -88,4 +101,31 @@ $app->put("/actualizarLibro/{referencia}",function($request){
     }
 });
 
+$app->delete("/borrarLibro/{referencia}",function($request){
+
+    $test=validateToken();
+    if(is_array($test))
+    {
+        if(isset($test["usuario"]))
+        {
+            if($test["usuario"]["tipo"]=="admin")
+            {
+                $respuesta=borrar_libro($request->getAttribute("referencia"));
+                $respuesta["token"]=$test["token"];
+                
+                echo json_encode($respuesta);
+            }
+            else
+                echo json_encode(array("no_auth"=>"No tienes permiso para usar el servicio"));  
+        }
+        else
+            echo json_encode($test);
+
+    }
+    else
+        echo json_encode(array("no_auth"=>"No tienes permiso para usar el servicio"));
+});
+
 $app->run();
+
+?>
--- dwese/Tema4/Pract_Final/js/index.js
+++ dwese/Tema4/Pract_Final/js/index.js
@@ -15,16 +15,16 @@ function cargar_libros_normal() {
                 $.each(data.libros, function (key, tupla) {
                     html_libros += "<div>";
                     html_libros += "<img src='images/" + tupla["portada"] + "' alt='Portada' title='Portada'><br>";
-                    html_libros += tupla["titulo"] + " - " + tupla["precio"] + "€";
+                    html_libros += tupla["titulo"] + " - " + tupla["precio"] + " €";
                     html_libros += "</div>";
                 });
-                html_libros += "</div>";
                 $('.contenedor_libros').html(html_libros);
             }
         })
         .fail(function (a, b) {
             $('#errores').html(error_ajax_jquery(a, b));
             $('#principal').html("");
+            localStorage.clear();
         });
 }
 
@@ -46,33 +46,89 @@ function cargar_libros_admin() {
                 $.each(data.libros, function (key, tupla) {
                     html_libros += "<tr>";
                     html_libros += "<td>" + tupla["referencia"] + "</td>";
-                    html_libros += "<td><button class='enlace' onClick='mostrar_detalles(" + tupla["referencia"] + ")'>" + tupla["titulo"] + "</button></td>";
-                    html_libros += "<td><button class='enlace' onClick='return false;'>Borrar</button> - <button class='enlace' onClick='return false;'>Editar</button></td>";
+                    html_libros += "<td><button class='enlace' onclick='mostrar_detalles(" + tupla["referencia"] + ")';'>" + tupla["titulo"] + "</button></td>";
+                    html_libros += "<td><button class='enlace' onclick='borrar_libro("+ tupla["referencia"] +");'>Borrar</button> - <button class='enlace' onclick='return false;'>Editar</button></td>";
                     html_libros += "</tr>";
                 });
-                html_libros += "</table>";
                 $('#libros').html(html_libros);
-
             }
         })
         .fail(function (a, b) {
             $('#errores').html(error_ajax_jquery(a, b));
             $('#principal').html("");
+            localStorage.clear();
         });
 }
 
-function mostrar_detalles(referencia) {
-    if (((new Date() / 1000) - localStorage.ultm_accion) < MINUTOS * 60) {
+function cargar_formulario_agregar() {
+    let html_form_agregar = "<h2>Agregar un nuevo Libro owo</h2>";
+    html_form_agregar += "<form id='form_agregar' action='#' method='post'>";
+    html_form_agregar += "<p><label for='referencia'>Referencia: </label>";
+    html_form_agregar += "<input type='text' id='referencia' name='referencia' required></p>";
+    html_form_agregar += "<p><label for='titulo'>Título:</label>";
+    html_form_agregar += "<input type='text' id='titulo' name='titulo' required></p>";
+    html_form_agregar += "<p><label for='autor'>Autor: </label>";
+    html_form_agregar += "<input type='text' id='autor' name='autor' required></p>";
+    html_form_agregar += "<p><label for='descripcion'>Descripción: </label>";
+    html_form_agregar += "<input type='text' id='descripcion' name='descripcion' required></p>";
+    html_form_agregar += "<p><label for='precio'>Precio: </label>";
+    html_form_agregar += "<input type='number' id='precio' name='precio' required></p>";
+    html_form_agregar += "<input type='submit' value='Agregar'>";
+    html_form_agregar += "</form>";
+    $('#respuestas').html(html_form_agregar);
+
+    // Manejar el envío del formulario
+    $('#form_agregar').on('submit', function (event) {
+        event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+        const formData = {
+            referencia: $('#referencia').val(),
+            titulo: $('#titulo').val(),
+            autor: $('#autor').val(),
+            descripcion: $('#descripcion').val(),
+            precio: $('#precio').val()
+        };
+
         $.ajax({
-            url: DIR_API + "/obtenerLibro/" + referencia,
-            type: "GET",
+            url: DIR_API + "/crearLibro",
+            type: "POST",
             dataType: "json",
-            headers: { Authorization: "Bearer " + localStorage.token }
+            data: formData,
+            headers: {
+                Authorization: "Bearer " + localStorage.token
+            }
         })
             .done(function (data) {
                 if (data.error) {
                     $('#errores').html(data.error);
                     $('#principal').html("");
+                    localStorage.clear();
+                } else {
+                    //alert('Libro agregado correctamente');
+                    $('#respuestas').html('Libro agregado correctamente :)');
+                    $('#form_agregar')[0].reset(); // Limpiar el formulario
+                }
+            })
+            .fail(function (a, b) {
+                $('#errores').html(error_ajax_jquery(a, b));
+                $('#principal').html("");
+            });
+    });
+}
+
+function mostrar_detalles(referencia) {
+
+    if (((new Date() / 1000) - localStorage.ultm_accion) < MINUTOS * 60) {
+        $.ajax({
+            url: DIR_API + "/obtenerLibro/" + referencia,
+            dataType: "json",
+            type: "GET",
+            headers: { Authorization: "Bearer " + localStorage.token }
+        })
+            .done(function (data) {
+                if (data.error) {
+                    $("#errores").html(data.error);
+                    $("#principal").html("");
                 }
                 else if (data.no_auth) {
                     localStorage.clear();
@@ -83,65 +139,57 @@ function mostrar_detalles(referencia) {
                     cargar_vista_login("Usted ya no se encuentra registrado en la BD.");
                 }
                 else {
-                    localStorage.setItem("ultm_accion", new Date() / 1000);
+                    localStorage.setItem("ultm_accion", (new Date() / 1000));
                     localStorage.setItem("token", data.token);
-                    let html_detalles_libro = "<h2>Detalles del libro" + referencia + "</h2>";
+
+                    let html_detalles_libro = "<h2>Detalles del Libro " + referencia + "</h2>";
                     html_detalles_libro += "<p>";
-                    html_detalles_libro += "<strong>Referencia:</strong> " + data.libro["referencia"] + "<br>";
-                    html_detalles_libro += "<strong>Título:</strong> " + data.libro["titulo"] + "<br>";
-                    html_detalles_libro += "<strong>Autor:</strong> " + data.libro["autor"] + "<br>";
-                    html_detalles_libro += "<strong>Descripción:</strong> " + data.libro["descripcion"] + "<br>";
-                    html_detalles_libro += "<strong>Precio:</strong> " + data.libro["precio"] + "€<br>";
-                    html_detalles_libro += "<img src='images/" + data.libro["portada"] + "' alt='Portada' title='Portada'><br>";
+                    html_detalles_libro += "<strong>Título:</strong>" + data.libro["titulo"] + "<br>";
+                    html_detalles_libro += "<strong>Autor:</strong>" + data.libro["autor"] + "<br>";
+                    html_detalles_libro += "<strong>Descripción:</strong>" + data.libro["descripcion"] + "<br>";
+                    html_detalles_libro += "<strong>Precio:</strong>" + data.libro["precio"] + " €<br>";
+                    html_detalles_libro += "<img src='images/" + data.libro["portada"] + "' alt='Portada' title='Portada'>";
                     html_detalles_libro += "</p>";
-                    html_detalles_libro += "<p><button onClick='cargar_formulario_agregar()'>Volver</button></p>";
-                    $("respuestas").html(html_detalles_libro);
+                    html_detalles_libro += "<p><button onclick='cargar_formulario_agregar()'>Volver</button></p>";
+
+                    $("#respuestas").html(html_detalles_libro);
+
                 }
+
             })
             .fail(function (a, b) {
-                $('#errores').html(error_ajax_jquery(a, b));
-                $('#principal').html("");
+                $("#errores").html(error_ajax_jquery(a, b));
+                $("#principal").html("");
+                localStorage.clear();
             });
-    } else {
-        localStorage.clear();
-        cargar_vista_login("Su tiempo e sesión ha cadicado");
     }
+    else {
+        localStorage.clear();
+        cargar_vista_login("Su tiempo de sesión ha expirado");
+    }
+
 }
 
-
-function cargar_formulario_agregar() {
-    let html_form_agregar = "<h2>Agregar un nuevo libro</h2>";
+function borrar_libro(cod) {
     $.ajax({
-        url: DIR_API + "/crearLibro",
-        type: "POST",
-        dataType: "json"
+        url: DIR_API + "/borrarLibro/" + cod,
+        dataType: "json",
+        type: "DELETE"
     })
         .done(function (data) {
             if (data.error) {
-                $('#errores').html(data.error);
-                $('#principal').html("");
-                localStorage.clear();
+                $("#errores").html(data.error);
+                $("#respuestas").html("");
+                $("#productos").html("");
             }
             else {
-                html_form_agregar += "<form id='form_agregar' action='#' method='post'>";
-                html_form_agregar += "<p><label for='referencia'>Referencia: </label>";
-                html_form_agregar += "<input type='text' id='referencia' name='referencia' required></p>";
-                html_form_agregar += "<p><label for='titulo'>Título:</label>";
-                html_form_agregar += "<input type='text' id='titulo' name='titulo' required></p>";
-                html_form_agregar += "<p><label for='autor'>Autor: </label>";
-                html_form_agregar += "<input type='text' id='autor' name='autor' required></p>";
-                html_form_agregar += "<p><label for='descripcion'>Descripción: </label>";
-                html_form_agregar += "<input type='text' id='descripcion' name='descripcion' required></p>";
-                html_form_agregar += "<p><label for='precio'>Precio: </label>";
-                html_form_agregar += "<input type='number' id='precio' name='precio' required></p>";
-                html_form_agregar += "<input type='submit' value='Agregar'>";
-                html_form_agregar += "</form>";
-                $('#respuestas').html(html_form_agregar);
+                $("#respuestas").html("<p class='txt_centrado mensaje'>¡¡ Libro borrado con éxito !!"+ cod +"</p>");
+                obtener_productos();
             }
         })
         .fail(function (a, b) {
-            $('#errores').html(error_ajax_jquery(a, b));
-            $('#principal').html("");
+            $("#errores").html(error_ajax_jquery(a, b));
+            $("#respuestas").html("");
+            $("#productos").html("");
         });
-
 }
